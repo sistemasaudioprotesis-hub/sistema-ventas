94ad48d2e52dbd362868986f681d12821f7a0b7a
sistemasaudioprotesis-hub <sistemasaudioprotesis@gmail.com>
2026-04-28 17:34:43 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,6 @@
 'use client'
 
 import { useEffect, useState } from 'react'
-import { supabase } from '../lib/supabaseClient'
 
 export default function Home() {
   const [permisos, setPermisos] = useState(null)
